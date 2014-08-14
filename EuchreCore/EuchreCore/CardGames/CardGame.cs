09bd26ab05ec690631d9a388e44b3e44928a4c52
Yi Li <yjli@umich.edu>
2014-08-13 18:52:38 -0700

    Added initial files
    Added initial files

---
+++ EuchreCore/EuchreCore/CardGames/CardGame.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EuchreCore.CardGames
+{
+    public class CardGame
+    {
+        public virtual void init()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void run()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void clear()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EuchreCore/EuchreCore/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EuchreCore.CardGames;
+
+namespace EuchreCore
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create the Game
+            CardGame game = new CardGame();
+
+            // initialize the game
+            game.init();
+
+            // run the game
+            game.run();
+
+            //exit
+            game.clear();
+        }
+    }
+}
