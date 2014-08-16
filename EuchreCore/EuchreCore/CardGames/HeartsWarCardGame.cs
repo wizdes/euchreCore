8b05c48a39e1b3f7a61e5a8c421d2c0866e24a27
Yi Li <yjli@umich.edu>
2014-08-16 12:07:30 -0700

    added players

--- EuchreCore/EuchreCore/CardGames/HeartsWarCardGame.cs
+++ EuchreCore/EuchreCore/CardGames/HeartsWarCardGame.cs
@@ -4,13 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EuchreCore.CardGameElements;
+using EuchreCore.PlayerClass;
 
 namespace EuchreCore.CardGames
 {
     class HeartsWarCardGame : CardGame
     {
         private Deck deck;
-        private List<PlayerHand> players;
+        private List<PlayerHand> playerHands;
+        private List<Player> players; 
 
         public override void init()
         {
@@ -21,14 +23,15 @@ namespace EuchreCore.CardGames
             // create the player hands
             for (int i = 0; i < 4; i++)
             {
-                players.Add(new PlayerHand());
+                playerHands.Add(new PlayerHand());
+                players.Add(new HeartsPlayer());
             }
         }
 
         public override void run()
         {
             // run the passing stage of the game
-            GameStage passingStage = new HeartsPassingGameStage(deck, players);
+            GameStage passingStage = new HeartsPassingGameStage(deck, playerHands);
 
             passingStage.run();
 
