3ce988a8a8c8a3a97c5527dc5eff53767bbe568d
Yi Li <yili@USS-Prometheus.local>
2014-09-20 10:34:11 -0700

    removed some unused references

--- EuchreCore/SparseGuiSite/Controllers/HomeController.cs
+++ EuchreCore/SparseGuiSite/Controllers/HomeController.cs
@@ -1,14 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
 using System.Threading;
-using System.Web;
 using System.Web.Mvc;
 using EuchreCore.CardGames;
 using EuchreCore.Interface;
 using EuchreCore.PlayerClass;
-using Microsoft.Ajax.Utilities;
 
 namespace SparseGuiSite.Controllers
 {
