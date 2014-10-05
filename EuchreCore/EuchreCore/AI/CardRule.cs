96f7658803a9e750134fea86e92ce2fb87e2971c
Yi Li <yili@USS-Prometheus.local>
2014-10-05 11:48:39 -0700

    added rules

---
+++ EuchreCore/EuchreCore/AI/CardRule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EuchreCore.CardGameElements;
+
+namespace EuchreCore.AI
+{
+    public abstract class CardRule
+    {
+        protected List<CardRule> nextRule;
+
+        public abstract Card Decide(List<Card> cards);
+    }
+}
---
+++ EuchreCore/EuchreCore/AI/RandomRule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EuchreCore.CardGameElements;
+
+namespace EuchreCore.AI
+{
+    public class RandomRule : CardRule
+    {
+        public override Card Decide(List<Card> cards)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
