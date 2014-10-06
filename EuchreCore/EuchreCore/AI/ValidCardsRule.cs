bc344fd08cfd517b6b98486d38c1520a1cc10359
Yi Li <yili@USS-Prometheus.local>
2014-10-05 22:57:53 -0700

    added function headers for AI

--- EuchreCore/EuchreCore/AI/ValidCardsRule.cs
+++ EuchreCore/EuchreCore/AI/ValidCardsRule.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,15 +9,11 @@ using EuchreCore.PlayerClass;
 
 namespace EuchreCore.AI
 {
-    public abstract class CardRule
+    public class ValidCardsRule : CardRule
     {
-        protected List<CardRule> nextRule;
-
-        public abstract Card Decide(List<Card> cards, GameState gamestate, Player player);
-
-        public void AddRule(CardRule r)
+        public override Card Decide(List<Card> cards, GameState gamestate, Player player)
         {
-            nextRule.Add(r);
+            throw new NotImplementedException();
         }
     }
 }
