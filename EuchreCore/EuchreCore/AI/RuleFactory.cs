1e8f01be5c35464c11ed8ac1e01faf120075fad8
Yi Li <yili@USS-Prometheus.local>
2014-10-10 11:36:44 -0700

    added a basic AI factory

---
+++ EuchreCore/EuchreCore/AI/RuleFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EuchreCore.AI
+{
+    public class RuleFactory
+    {
+        public CardRule GetBasicAI()
+        {
+            CardRule rule = new ValidCardsRule();
+            CardRule nextRule = new GetLowestCardsRule();
+            CardRule randomRole = new RandomRule();
+
+            nextRule.AddRule(randomRole);
+            randomRole.AddRule(nextRule);
+
+            return rule;
+        }
+    }
+}
